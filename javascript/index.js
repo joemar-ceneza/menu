a4813baa61c81e3573aa91954d55e4496c2ad366
joemar-ceneza <joemar.ceneza@gmail.com>
2022-09-27 08:22:54 +0800

    add some querySelector's

--- javascript/index.js
+++ javascript/index.js
@@ -80,3 +80,6 @@ const menu = [
         desc: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.`
     },
 ];
+
+const sectionCenter = document.querySelector(".section-center");
+const btnContainer = document.querySelector(".btn-container");
