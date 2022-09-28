bd0f52109754038e1b066e612023a4107cae93af
joemar-ceneza <joemar.ceneza@gmail.com>
2022-09-28 08:25:12 +0800

    add some code

--- javascript/index.js
+++ javascript/index.js
@@ -83,3 +83,24 @@ const menu = [
 
 const sectionCenter = document.querySelector(".section-center");
 const btnContainer = document.querySelector(".btn-container");
+
+window.addEventListener("DOMContentLoaded", function(){
+
+});
+
+function displayMenuItems(menuItems){
+    let displayMenu = menuItems.map(function (item){
+        return `<article class="menu-item">
+        <img src=${item.img} alt=${item.title} class="photo" />
+        <div class="item-info">
+          <header>
+            <h4>${item.title}</h4>
+            <h4 class="price">$${item.price}</h4>
+          </header>
+          <p class="item-text">
+            ${item.desc}
+          </p>
+        </div>
+      </article>`;
+    });
+}
